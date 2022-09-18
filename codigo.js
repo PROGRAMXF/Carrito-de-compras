379994b40ebbe0b5f6cb6654640ea4b59ac3da31
Fernando <fh1273509@gmail.com>
2022-09-18 20:20:40 -0300

    creamos las tarjetas de cada articulo

--- codigo.js
+++ codigo.js
@@ -19,3 +19,50 @@ function listSelect(){
         
     })
 }
+//creamos las tarjetas
+function crearTarjetas(herramienta){
+    const {articulo, imagen, id, precio} = herramienta;
+
+    //creamos el contenedor
+    const tarjeta = document.createElement('div');
+
+    //me permite agregar una clase:
+    tarjeta.classList.add('tarjeta-art');
+
+    //lo mismo con las imagenes:
+    const imagenTarjeta = document.createElement('img');
+
+    //le agrego dos atributos: la direccion y la foto
+    imagenTarjeta.setAttribute('src', imagen);
+
+    //otro dos atributos: alt: texto alternativo y una concatenacion para que nos traiga el id y el articulo
+    imagenTarjeta.setAttribute('alt', `${id}-${articulo}`);
+
+    //creamos otra clase
+    imagenTarjeta.classList.add('img-articulo');
+
+    //ahora vamos con la clase del nombre del producto con un parametro p(parrafo)
+    const nombreTarjeta = document.createElement('p');
+
+    //definimos el contenido que va a tener el parrafo y su respectiva clase:   
+    nombreTarjeta.textContent = articulo;
+    nombreTarjeta.classList.add('nombre-articulo');
+
+    //hacemos lo mismo para el precio:
+    const precioTarjeta = document.createElement('p');
+
+    precioTarjeta.textContent = precio;
+    precioTarjeta.classList.add('precio-tarjeta');
+
+
+    //creamos un boton:
+    const btnAdd = document.createElement('button');
+
+    //creamos un atributo para reconocer la imagen seleccionada a traves del id del objeto o articulo
+    btnAdd.setAttribute('id', id);
+
+    //la aplicamos una clase al boton y su respectivo texto:
+    btnAdd.classList.add('btn-add')
+    btnAdd.textContent = "Agregar al Carrito";
+
+}
