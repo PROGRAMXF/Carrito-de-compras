765cb5839f16f3b345543b814079dfb906674e45
Fernando <fh1273509@gmail.com>
2022-09-18 22:56:49 -0300

    renderizado de articulos

--- codigo.js
+++ codigo.js
@@ -5,6 +5,15 @@ const selectorArticulos = document.getElementById('selec-art');
 window.addEventListener('load', listSelect);
 //listSelect me mapea todo el array de articulos y para ello creamos una función:
 
+//vamos a crear la funcion de renderizar
+//listSelect se encarga de listar los elementos en windows
+selectorArticulos.addEventListener('change', renderCards);
+
+function renderCards(){
+    //ejercicio de validacion:
+    herramientas.map(herr =>{ herr.articulo === selectorArticulos.value ? crearTarjetas(herr): null});
+}
+
 function listSelect(){
     herramientas.map(herr =>{
         //creamos una etiqueta opción que la referenciamos en el index "Elejir una opcion"
@@ -14,14 +23,12 @@ function listSelect(){
         option.value = herr.articulo; // me trae cada producto
         option.textContent = herr.articulo; // me lee cada producto
 
-        selectorArticulos.appendChild(option);//coloca un elemento hijo
-
-        
+        selectorArticulos.appendChild(option);//coloca un elemento hijo        
     })
 }
 //creamos las tarjetas
-function crearTarjetas(herramienta){
-    const {articulo, imagen, id, precio} = herramienta;
+function crearTarjetas(herr){
+    const {articulo, imagen, id, precio} = herr;
 
     //creamos el contenedor
     const tarjeta = document.createElement('div');
@@ -38,6 +45,8 @@ function crearTarjetas(herramienta){
     //otro dos atributos: alt: texto alternativo y una concatenacion para que nos traiga el id y el articulo
     imagenTarjeta.setAttribute('alt', `${id}-${articulo}`);
 
+
+
     //creamos otra clase
     imagenTarjeta.classList.add('img-articulo');
 
@@ -52,7 +61,8 @@ function crearTarjetas(herramienta){
     const precioTarjeta = document.createElement('p');
 
     precioTarjeta.textContent = precio;
-    precioTarjeta.classList.add('precio-tarjeta');
+    precioTarjeta.classList.add('precio-articulo');
+
 
 
     //creamos un boton:
@@ -65,4 +75,17 @@ function crearTarjetas(herramienta){
     btnAdd.classList.add('btn-add')
     btnAdd.textContent = "Agregar al Carrito";
 
+
+
+    //Ahora vamos agregando los elementos:
+    // a las tarjetas que son el contenedor principal le vamos agregando los hijos con los appendChild()
+
+    tarjeta.appendChild(imagenTarjeta);
+    tarjeta.appendChild(nombreTarjeta);
+    tarjeta.appendChild(precioTarjeta);
+    tarjeta.appendChild(btnAdd);
+
+
+    //de esta manera lo vinculamos al main del index
+    contenedorImagenes.appendChild(tarjeta);
 }
