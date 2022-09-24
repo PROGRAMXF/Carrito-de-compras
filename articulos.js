af82c29486d9fbbe2ac959f38877a376fd302be7
Fernando <fh1273509@gmail.com>
2022-09-23 23:39:43 -0300

    imagenes no mostradas solucionado era agregar dos puntos(..)

--- articulos.js
+++ articulos.js
@@ -1,14 +1,14 @@
 const herramientas = [
 
     {id:"herr-01", articulo:"Pinza", precio:350, imagen:'../assets/Pinza.png'},
-    {id:"herr-02", articulo:"Alicate", precio:370, imagen:'/assets/Alicate.jpg'},
-    {id:"herr-03", articulo:"Pico de loro", precio:650, imagen:'/assets/PicoDeLoro.png'}, 
-    {id:"herr-04", articulo:"Destornillador", precio:230, imagen:'/assets/Destornillador.png'},
-    {id:"herr-05", articulo:"Saca Bujia", precio:1500, imagen:'/assets/SacaBujia.jpg'},
-    {id:"herr-06", articulo:"Martillo", precio:850, imagen:'/assets/Martillo.jpg'},
-    {id:"herr-07", articulo:"Lima", precio:400, imagen:'/assets/Lima.jpg'},
-    {id:"herr-08", articulo:"Lija", precio:120, imagen:'/assets/Lija.webp'},
-    {id:"herr-09", articulo:"Mecha", precio:180, imagen:'/assets/Mecha.jpg'},
-    {id:"herr-10", articulo:"Metro", precio:720, imagen:'/assets/Metro.webp'},
+    {id:"herr-02", articulo:"Alicate", precio:370, imagen:'../assets/Alicate.jpg'},
+    {id:"herr-03", articulo:"Pico de loro", precio:650, imagen:'../assets/PicoDeLoro.png'}, 
+    {id:"herr-04", articulo:"Destornillador", precio:230, imagen:'../assets/Destornillador.png'},
+    {id:"herr-05", articulo:"Saca Bujia", precio:1500, imagen:'../assets/SacaBujia.jpg'},
+    {id:"herr-06", articulo:"Martillo", precio:850, imagen:'../assets/Martillo.jpg'},
+    {id:"herr-07", articulo:"Lima", precio:400, imagen:'../assets/Lima.jpg'},
+    {id:"herr-08", articulo:"Lija", precio:120, imagen:'../assets/Lija.webp'},
+    {id:"herr-09", articulo:"Mecha", precio:180, imagen:'../assets/Mecha.jpg'},
+    {id:"herr-10", articulo:"Metro", precio:720, imagen:'../assets/Metro.webp'},
 
 ]
