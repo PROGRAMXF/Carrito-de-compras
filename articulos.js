909c675aae6c68961dbeab4367d861bb26965d86
Fernando <fh1273509@gmail.com>
2022-09-23 23:28:53 -0300

    modificando imagen sacabujia

--- articulos.js
+++ articulos.js
@@ -4,7 +4,7 @@ const herramientas = [
     {id:"herr-02", articulo:"Alicate", precio:370, imagen:'/assets/Alicate.jpg'},
     {id:"herr-03", articulo:"Pico de loro", precio:650, imagen:'/assets/PicoDeLoro.png'}, 
     {id:"herr-04", articulo:"Destornillador", precio:230, imagen:'/assets/Destornillador.png'},
-    {id:"herr-05", articulo:"Saca Bujia", precio:1500, imagen:'/assets/SacaBujia.jpg'},
+    {id:"herr-05", articulo:"Saca Bujia", precio:1500, imagen:'/SacaBujia.jpg'},
     {id:"herr-06", articulo:"Martillo", precio:850, imagen:'/assets/Martillo.jpg'},
     {id:"herr-07", articulo:"Lima", precio:400, imagen:'/assets/Lima.jpg'},
     {id:"herr-08", articulo:"Lija", precio:120, imagen:'/assets/Lija.webp'},
